ef845333352941a2298bc566c95ddf81c56940ee
ivan <ivanmontanesfilipit@gmail.com>
2025-05-25 17:04:12 -0300

    ejercicio 4 subcadena terminado

--- Program.cs
+++ Program.cs
@@ -11,3 +11,21 @@ Console.WriteLine("ingrese otra cadena:");
 cadena2 = Console.ReadLine();
 // concatenamos ambas cadenas usando + //
 Console.WriteLine("Cadena Concatenada:" + cadena + cadena2);
+// extraemos una subcadena de la cadena ingresada (la primera) //
+int indice = default;
+do
+{
+    Console.WriteLine($"ingrese el indice desde donde desea obtener la subcadena de la palabra {cadena}:");
+} while (!int.TryParse(Console.ReadLine(), out indice));
+// verificamos que el indice ingresado sea mayor o igual que 0 y menor que el lenght de la cadena //
+if (!(indice >= 0 && indice < cadena.Length))
+{
+    Console.WriteLine("no se puede obtener una subcadena con ese indice");
+}
+else
+{
+    // usamos el metodo substring //
+    string subcadena = cadena.Substring(indice);
+    Console.WriteLine($"Subcadena: {subcadena}");
+}
+
