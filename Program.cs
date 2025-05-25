eea40592d8b693d63fd80fc7f8b65336416ed12f
ivan <ivanmontanesfilipit@gmail.com>
2025-05-25 16:44:38 -0300

    ejercicio 4 longitud cadena, y concatenar cadenas terminado

--- Program.cs
+++ Program.cs
@@ -1,37 +1,13 @@
-﻿// logica para cargar el numero //
-// variables para cargar el numero //
-int numero;
-bool resultadoConversion;
-// bucle para cargar el numero //
-do
-{
-    // pedimos el numero por consola //
-    Console.WriteLine("Ingrese un numero");
-    string numeroCadena = Console.ReadLine();
-    // tratamos de convertir la cadena a un numero //
-    resultadoConversion = int.TryParse(numeroCadena, out numero);
-    if (!resultadoConversion)
-    {
-        Console.WriteLine("no se pudo convertir la cadena a un numero");
-    }
-} while (!resultadoConversion);
-
-// logica para invertir el numero solamente si es mayor que 0 //
-if (numero > 0)
-{
-    // variables que vamos a utilizar para invertir el numero //
-    int resto, cociente = numero, invertido = 0;
-    do
-    {
-        // dividimos el numero entre 10 para obtener el resto o el ultimo digito //
-        resto = cociente % 10;
-        // calculamos el cociente para eliminar el ultimo numero //
-        cociente /= 10;
-        invertido = invertido * 10 + resto;
-    } while (cociente != 0);
-    Console.WriteLine($"Numero Invertido: {invertido}");
-}
-else
-{
-    Console.WriteLine("el numero es menor o igual a 0");
-}
+﻿// pedimos la cadena al usuario //
+string cadena = string.Empty;
+Console.WriteLine("ingrese la cadena:");
+cadena = Console.ReadLine();
+// mostramos la longitud de la cadena //
+int longitudCadena = cadena.Length;
+Console.WriteLine($"Longitud de la cadena: {longitudCadena} caracteres");
+// pedimos una segunda cadena //
+string cadena2 = string.Empty;
+Console.WriteLine("ingrese otra cadena:");
+cadena2 = Console.ReadLine();
+// concatenamos ambas cadenas usando + //
+Console.WriteLine("Cadena Concatenada:" + cadena + cadena2);
